679c8995d93ff873d89d52b3c3afe2abb984ed79
ahmed elhatab <ahmedelhatab@gmail.com>
2020-10-12 13:03:26 +0200

    GUI file

--- GUI.py
+++ GUI.py
@@ -1,6 +1,20 @@
-import threadsafe_tkinter as tk;
-main_window=tk.Tk();
+import threadsafe_tkinter as tk
+import tkinter as tkm
+from tkinter.font import *;
+main_window=tkm.Tk()
 #set Title for main window.
 main_window.title("Application Development ")
 #change the icon of the window.
-main_window.mainloop();
+#main_window.iconphoto("E:\\Photos\\test.ico");
+#font for GUI elements
+font=Font(family="Calibri",size=12);
+#create a label
+label=tk.Label(main_window,text="User National ID :",font=font)
+#arrange the elements in grid.
+label.grid(row=0,column=0)
+#change the color of the label.
+label.config(fg="#FF00FF");
+#change the font
+
+label.config()
+main_window.mainloop()
