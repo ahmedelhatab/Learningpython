6fb4ed4c856b362f85ba36dfe954e5baa7fe53d4
ahmed elhatab <ahmedelhatab@gmail.com>
2020-10-12 10:14:07 +0200

    Initial Commit

---
+++ GUI.py
@@ -0,0 +1,6 @@
+import threadsafe_tkinter as tk;
+main_window=tk.Tk();
+#set Title for main window.
+main_window.title("Application Development ")
+#change the icon of the window.
+main_window.mainloop();
