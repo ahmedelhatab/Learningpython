3079ce96273f0822cae65cfb5f9951c331c7da8a
ahmed elhatab <ahmedelhatab@gmail.com>
2020-10-21 14:03:28 +0200

    Radio Button

--- DataTypes.py
+++ DataTypes.py
@@ -1,16 +1,17 @@
 import sys
 import timeit
+
 # Lists
-my_first_list=["java","R","Python"]
+my_first_list = ["java", "R", "Python"]
 print(my_first_list)
-  #Empty List
-my_empty_list=list()
+# Empty List
+my_empty_list = list()
 print(my_empty_list)
 my_first_list.append("Kotlin")
 print(my_first_list)
-my_first_list.insert(3,"NJ")
+my_first_list.insert(3, "NJ")
 print(my_first_list)
-last_element=my_first_list.pop()
+last_element = my_first_list.pop()
 print(last_element)
 print(my_first_list)
 my_first_list.remove("java")
@@ -20,28 +21,28 @@ print(my_first_list)
 my_empty_list.append(3)
 my_empty_list.append(5)
 print(my_empty_list)
-my_second_list=[1,2,-1,-5,-10,20]
+my_second_list = [1, 2, -1, -5, -10, 20]
 print(my_second_list)
 my_second_list.sort()
 print(my_second_list)
 my_second_list.reverse()
 print(my_second_list)
-my_third_list=my_first_list+my_second_list
+my_third_list = my_first_list + my_second_list
 print(my_third_list)
-  #slicing
+# slicing
 print(my_third_list[::-1])
-my_first_list=my_third_list.copy()
+my_first_list = my_third_list.copy()
 my_first_list.append("Crazy")
 print(my_first_list)
-sequare_list=[i*2 for i in my_second_list]
-print(sequare_list)
-#Tuples
-my_first_tuples=("java","python","R")
+square_list = [i * 2 for i in my_second_list]
+print(square_list)
+# Tuples
+my_first_tuples = ("java", "python", "R")
 print(type(my_first_tuples))
-number_of_java=my_first_tuples.count("java")
-print("The number of elements "+str(number_of_java))
+number_of_java = my_first_tuples.count("java")
+print("The number of elements " + str(number_of_java))
 my_first_tuples.index("java")
-my_second_tuples=tuple(my_first_list)
+my_second_tuples = tuple(my_first_list)
 print(my_first_tuples.index("java"))
 print(my_first_tuples.__repr__())
 for i in my_first_tuples:
@@ -51,10 +52,34 @@ print(my_second_tuples)
 
 if "R" in my_second_tuples:
     print(my_second_tuples.index("R"))
-#unpacking using argument
-my_third_tuples=("java","python","C++","MMM")
-i1,*i2,i3=my_third_tuples
+# unpacking using argument
+my_third_tuples = ("java", "python", "C++", "MMM")
+i1, *i2, i3 = my_third_tuples
 print(i2)
-print(sys.getsizeof(my_third_list),"Megabytes")
-print(timeit.timeit(stmt="[0,1,2,3,4]",number=1000000))
-print(timeit.timeit(stmt="(0,1,2,3,4)",number=1000000))
+print(sys.getsizeof(my_third_list), "Megabytes")
+print(timeit.timeit(stmt="[0,1,2,3,4]", number=1000000))
+print(timeit.timeit(stmt="(0,1,2,3,4)", number=1000000))
+# Dictionary
+my_first_dictionary = {"age": "23", "name": "amr"}
+print(my_first_dictionary)
+my_second_dictionary = dict(name="Ahmed Zakaria ", age=33, date="2020-5-15")
+print(my_second_dictionary)
+name = my_second_dictionary["name"]
+print(name)
+# TODO check unpacking for dictionary
+my_second_dictionary["email"] = "ahmad.ali@te.eg"
+print(my_second_dictionary)
+email = my_second_dictionary.pop("email")
+print(email)
+print(my_second_dictionary.popitem())
+if "name" in my_second_dictionary:
+    print(my_second_dictionary["name"])
+for key, value in my_second_dictionary.items():
+    print(f'{0}:{1} are very crititcal', {key}, {value})
+my_third_dictioanry = dict(my_second_dictionary)
+my_third_dictioanry["manager"] = "Maher Al Molla"
+print(my_third_dictioanry)
+interim_dict=my_second_dictionary.update(my_third_dictioanry)
+my_dictionaries = my_first_dictionary.update(
+    interim_dict)
+print(my_dictionaries)
--- GUI.py
+++ GUI.py
@@ -1,20 +1,49 @@
-import threadsafe_tkinter as tk
-import tkinter as tkm
-from tkinter.font import *;
-main_window=tkm.Tk()
-#set Title for main window.
-main_window.title("Application Development ")
-#change the icon of the window.
-#main_window.iconphoto("E:\\Photos\\test.ico");
-#font for GUI elements
-font=Font(family="Calibri",size=12);
-#create a label
-label=tk.Label(main_window,text="User National ID :",font=font)
-#arrange the elements in grid.
-label.grid(row=0,column=0)
-#change the color of the label.
-label.config(fg="#FF00FF");
-#change the font
+from tkinter import *
+from tkinter.font import *
+from PIL import ImageTk,Image ;
 
+
+
+
+main_window = Tk()
+bt_image=ImageTk.PhotoImage(Image.open("./search.png"))
+main_window.iconbitmap("./RA.gif")
+# set Title for main window.
+main_window.title("Products & Systems Assurance ")
+# change the icon of the window.
+# main_window.iconphoto("E:\\Photos\\test.ico");
+# font for GUI elements
+font = Font(family="Calibri", size=12)
+# create a label
+label = Label(main_window, text="User National ID :", font=font)
+# arrange the elements in grid.
+label.grid(row=0, column=0)
+# change the color of the label.
+label.config(fg="#FF00FF")
+# change the font
+user_Nation_id_box = Entry( fg="#0000FF",
+                           width=14,borderwidth=5)
+user_Nation_id_box.insert(0,"Enter your national ID")
+user_Nation_id_box.grid(row=0, column=1)
 label.config()
+#lablel frame
+our_label=LabelFrame(text="User Information")
+our_label.grid(row=2,column=0)
+sys_name=StringVar()
+sys_name.set("IM")
+button_forth=Button(our_label,text="Group")
+button_forth.grid(row=0,column=0)
+am_radio_button=Radiobutton(our_label,variable=sys_name,value="AM",text="AM")
+am_radio_button.grid(row=1,column=0);
+im_radio_button=Radiobutton(our_label,variable=sys_name,value="IM",text="IM")
+im_radio_button.grid(row=1,column=1);
+om_radio_button=Radiobutton(our_label,variable=sys_name,value="OM",text="OM")
+om_radio_button.grid(row=1,column=2);
+def my_click():
+    print(user_Nation_id_box.get())
+# Button
+search_Button = Button(text="Search ", padx=10, command=my_click, fg="#FF0000",
+                       bg="#FFFFFF",image=bt_image)
+search_Button.grid(row=0, column=2)
+#main_window.iconbitmap('./risk.ico')
 main_window.mainloop()
