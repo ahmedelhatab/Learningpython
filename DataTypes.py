9b26cade345d393cfe8c9286e7027d38bafb60c8
ahmed elhatab <ahmedelhatab@gmail.com>
2020-10-22 10:29:28 +0200

    itertools and collections

--- DataTypes.py
+++ DataTypes.py
@@ -1,6 +1,7 @@
+from itertools import *
 import sys
 import timeit
-
+from collections import *
 # Lists
 my_first_list = ["java", "R", "Python"]
 print(my_first_list)
@@ -79,7 +80,86 @@ for key, value in my_second_dictionary.items():
 my_third_dictioanry = dict(my_second_dictionary)
 my_third_dictioanry["manager"] = "Maher Al Molla"
 print(my_third_dictioanry)
-interim_dict=my_second_dictionary.update(my_third_dictioanry)
-my_dictionaries = my_first_dictionary.update(
-    interim_dict)
-print(my_dictionaries)
+# interim_dict=my_second_dictionary.update(my_third_dictioanry)
+# my_dictionaries = my_first_dictionary.update(
+#    interim_dict)
+# print(my_dictionaries)
+# Sets
+my_first_set = {1, 1, 2, 3}
+print(my_first_set)
+my_first_set.add(55)
+print(my_first_set)
+my_first_set.discard(550)
+print(my_first_set)
+my_first_set.discard(55)
+print(my_first_set)
+my_second_set = {2, 3, 4, 5}
+print(my_first_set.union(my_second_set))
+print(my_second_set.intersection(my_first_set))
+print(my_second_set.difference(my_first_set))
+print(my_second_set.symmetric_difference(my_first_set))
+print(my_first_set.intersection_update(my_second_set))
+print(my_first_set.issubset(my_second_set))
+#Counter
+my_String="aaaaaaaaAAAAAAAAAAAAZZZZZZZZZZZZZz"
+my_first_counter=Counter(my_String);
+print(my_first_counter)
+print(my_first_counter.keys())
+print(my_first_counter.values())
+print(type(my_first_counter.most_common()))
+print(my_first_counter.elements())
+print(my_first_counter.items())
+#Named Tuples
+Person=namedtuple('Person','name,age,place_of_birth')
+heba=Person("Heba","23","Cairo");
+print(heba[0]);
+my_first_ordered_dict=OrderedDict();
+my_first_ordered_dict["Ahmed"]=1
+my_first_ordered_dict["Christine"]=2
+print(my_first_ordered_dict)
+#Default Dictionary
+my_second_default_dict=defaultdict(int);
+my_second_default_dict["Ahmed"]=10
+my_second_default_dict["Ali"]=2
+print(my_second_default_dict)
+my_first_deaue=deque()
+my_first_deaue.append(0)
+my_first_deaue.append(1)
+print(my_first_deaue)
+my_first_deaue.extendleft([4,5,5,5,5])
+print(my_first_deaue)
+my_first_deaue.rotate(3)
+print(my_first_deaue)
+#rotate left
+my_first_deaue.rotate(-3)
+print(my_first_deaue)
+#product from itertools
+list1=[1,2,3]
+list2=[3,4]
+my_first_product=product(list1,list2)
+print(list(my_first_product))
+my_second_product=product(list1,list2,repeat=3)
+print(list(my_second_product))
+my_first_permutation=permutations(list1)
+print(list(my_first_permutation))
+my_first_combination=combinations(list1,r=2)
+print("####combination####")
+print(list(my_first_combination))
+#combination with replacement
+my_first_replace_comb=combinations_with_replacement(list1,r=2)
+
+print(list(my_first_replace_comb))
+my_acc=accumulate(list1)
+print(list(my_acc))
+for x in count(10):
+    print(x)
+    if x==130:
+        break;
+for x in cycle(list1):
+    print(x);
+    if(x==3):
+     break
+for x in repeat(1):
+    print(x)
+    if(x==1):
+        break
